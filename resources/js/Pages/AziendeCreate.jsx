90f3a0b4c29f2ea4b3fd87f7c9ade565848b7687
nicola <nicola.melito@gbrain.it>
2024-07-16 22:22:59 +0200

    Revert "multi step pagination"
    This reverts commit 537d16ab207f2d7c8d54f7ade8037c4f5303a0b7.

--- resources/js/Pages/AziendeCreate.jsx
+++ resources/js/Pages/AziendeCreate.jsx
@@ -6,14 +6,6 @@ import {useState} from 'react';
 
 export default function create({auth, aziende}) {
 
-    const [step, setStep] = useState(1);
-    const handleNext = () => {
-        setStep(step + 1);
-    };
-
-    const handleBack = () => {
-        setStep(step - 1);
-    };
 
     let id;
     let correlationId;
@@ -74,7 +66,7 @@ export default function create({auth, aziende}) {
         safeNo: '',
         simpleValue: '',
         activityCode: '',
-        correlationId: '',
+        correlationId:'',
     });
     const handleSubmitVatNo = (e) => {
         e.preventDefault();
@@ -108,31 +100,33 @@ export default function create({auth, aziende}) {
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                             <form className="grid grid-cols-1 sm:grid-cols-2 gap-6" onSubmit={handleSubmitVatNo}>
-                                <form className="grid grid-cols-1 sm:grid-cols-2 gap-6" onSubmit={handleSubmit}>
-                                    {step === 1 && (
-                                        <>
-                                            <div className="mb-4">
-                                                <InputLabel htmlFor="vatNo" value="Vat Number"/>
-                                                <TextInput
-                                                    id="vatNo"
-                                                    name="vatNo"
-                                                    value={data.vatNO}
-                                                    onChange={(e) => setData('vatNO', e.target.value)}
-                                                    type="text"
-                                                    className="mt-1 block w-full"
-                                                    placeholder="Vat Number"
-                                                />
-                                                {errors.vatNO && <div className="text-red-600">{errors.vatNO}</div>}
-                                            </div>
-                                            <div className="flex items-center justify-between col-span-1 sm:col-span-2">
-                                                <button
-                                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                                    type="submit"
-                                                    disabled={processing}
-                                                >
-                                                    Invia
-                                                </button>
-                                            </div>
+                                <div className="mb-4">
+                                    <InputLabel htmlFor="vatNo" value="Vat Number"/>
+                                    <TextInput
+                                        id="vatNo"
+                                        name="vatNo"
+                                        value={data.vatNO}
+                                        onChange={(e) => setData('vatNO', e.target.value)}
+                                        type="text"
+                                        className="mt-1 block w-full"
+                                        placeholder="Vat Number"
+                                    />
+                                    {errors.vatNO && <div className="text-red-600">{errors.vatNO}</div>}
+                                </div>
+                                <div className="flex items-center justify-between col-span-1 sm:col-span-2">
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                        type="submit"
+                                        disabled={processing}
+                                    >
+                                        Invia
+                                    </button>
+                                </div>
+                            </form>
+                            {
+                                data.vatNO !== '' ?
+                                    <>
+                                        <form className="grid grid-cols-1 sm:grid-cols-2 gap-6" onSubmit={handleSubmit}>
                                             <div className="mb-4">
                                                 <InputLabel htmlFor="activityCode" value="activityCode"/>
                                                 <TextInput
@@ -300,72 +294,26 @@ export default function create({auth, aziende}) {
                                                 {errors.safeNo && <div className="text-red-600">{errors.safeNo}</div>}
                                             </div>
 
+
                                             <div className="flex items-center justify-between col-span-1 sm:col-span-2">
                                                 <button
                                                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                                    type="button"
-                                                    onClick={handleNext}
+                                                    type="submit"
+                                                    disabled={processing}
                                                 >
-                                                    Next
+                                                    Invia
                                                 </button>
                                             </div>
-                                        </>
-                                    )}
-                                    {step === 2 && (
-                                        <>
-
-                                            <div className="flex items-center justify-between col-span-1 sm:col-span-2">
-                                                <button
-                                                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                                    type="button"
-                                                    onClick={handleBack}
-                                                >
-                                                    Back
-                                                </button>
-                                                <button
-                                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                                    type="button"
-                                                    onClick={handleNext}
-                                                >
-                                                    Next
-                                                </button>
-                                            </div>
-                                        </>
-                                    )}
-
-                                    {step === 3 && (
-                                        <>
-
-                                            <div className="flex items-center justify-between col-span-1 sm:col-span-2">
-                                                <button
-                                                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                                    type="button"
-                                                    onClick={handleBack}
-                                                >
-                                                    Back
-                                                </button>
-                                                <div
-                                                    className="flex items-center justify-between col-span-1 sm:col-span-2">
-                                                    <button
-                                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                                        type="submit"
-                                                        disabled={processing}
-                                                    >
-                                                        Invia
-                                                    </button>
-                                                </div>
-
-                                            </div>
-                                        </>
-                                    )}
-                                </form>
-                            </form>
-
+                                        </form>
+                                    </>
+                                    :
+                                    <>
+                                    </>
+                            }
                         </div>
                     </div>
                 </div>
             </div>
         </AuthenticatedLayout>
-    )
-        ;
+    );
 }
